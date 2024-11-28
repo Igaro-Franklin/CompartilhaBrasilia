6137dff2416dd515efa7eb64460fd56916269c63
Igaro-Franklin <igarofranklin@gmail.com>
2024-11-27 21:58:37 -0300

    Adicionou validação no formulario

--- src/app/components/evento-form/evento-form.component.ts
+++ src/app/components/evento-form/evento-form.component.ts
@@ -1,19 +1,47 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './evento-form.component.html',
-  styleUrl: './evento-form.component.css'
+  styleUrls: ['./evento-form.component.css']
 })
-export class EventoFormComponent {
-  @Input() btnText!: String
+export class EventoFormComponent implements OnInit {
+  @Input() btnText!: string;
 
-  constructor(){
+  eventoForm!: FormGroup;
+  formSubmitted = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.eventoForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
   }
 
-  ngOnInit(): void{
+  get title() {
+    return this.eventoForm.get('title')!;
+  }
 
+  get description() {
+    return this.eventoForm.get('description')!;
+  }
+
+  submit() {
+    this.formSubmitted = true;
+
+    if (this.eventoForm.valid) {
+      console.log("Formulário enviado com sucesso:", this.eventoForm.value);
+    } else {
+      console.log("Formulário inválido");
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     FormsModule,
     ReactiveFormsModule,
+    provideRouter(routes),
+    FormGroup,
   ]
 }).catch((err) => console.error(err));
